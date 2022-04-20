d87c4e736e8da5c053d6f2e47692790f3e905df2
ShrutiUjlan <shrutiujlan1@gmail.com>
2022-04-20 16:47:48 +0530

    [shruti][fullstack-app]

---
+++ src/main/java/StudentManagementSystem/project/controller/UIController.java
@@ -0,0 +1,14 @@
+package StudentManagementSystem.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UIController {
+
+
+	@GetMapping(path = {"/","/students","/add-student/**","/view-student/**",})
+	public String getView() {
+		return "index";
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO STUDENT VALUES (10, 'shrutiujlan@gmail.com','shruti', 'ujlan');
+INSERT INTO STUDENT VALUES (11,  'shrishtisharma@gmail.com','shrishti', 'sharma');
+INSERT INTO STUDENT VALUES (12, 'shrishtisharma@gmail.com','daksh', 'verma');
+INSERT INTO STUDENT VALUES (13, 'shrishtisharma@gmail.com', 'sanjeev', 'poduri');
+INSERT INTO STUDENT VALUES (14,  'nh@gmail.com','nate', 'holmes');
