f0836398a1fc68e9a39f8776be574b478c677120
ShrutiUjlan <shrutiujlan1@gmail.com>
2022-04-25 12:29:52 +0530

    rollback @Restcontroller changes

--- src/main/java/StudentManagementSystem/project/controller/UIController.java
+++ src/main/java/StudentManagementSystem/project/controller/UIController.java
@@ -3,7 +3,7 @@ package StudentManagementSystem.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class UIController {
 
 
